1ce7583d65e7e7da64cd8f2bb8ed03ac46ba7c4d
Drile99 <nikoladrikic5@gmail.com>
2021-11-10 09:53:04 +0100

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,123 @@
+
+import { Component, OnInit } from '@angular/core';
+ 
+ 
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+ 
+ //inicijalizovanje i deklarisanje vrednosti sa html strane
+  public input:string = '';
+  public result:string = '';
+  public histr:string[] = [];
+  public lis: number=0;
+
+
+  public ngOnInit(): void {
+    this.histr = JSON.parse(localStorage.getItem('lista') || '{}');
+  }
+ 
+  public pressNum(num: string): void {
+    
+    //zabrana dodavanje . vise od jedanput
+    if (num==".") {
+      if (this.input !="" ) {
+ 
+        const lastNum=this.getLastOperand()
+        console.log(lastNum.lastIndexOf("."))
+        if (lastNum.lastIndexOf(".") >= 0) return;
+      }
+    }
+ 
+    //Zabrana stavljanje 0 na pocetak
+    if (num=="0") {
+      if (this.input=="" ) {
+        return;
+      }
+      const PrevKey = this.input[this.input.length - 1];
+      if (PrevKey === '/' || PrevKey === '*' || PrevKey === '-' || PrevKey === '+')  {
+          return;
+      }
+    }
+ 
+    this.input = this.input + num
+    this.calcAnswer();
+  }
+ 
+ 
+  getLastOperand() {
+    let pos:number;
+    console.log(this.input)
+    pos=this.input.toString().lastIndexOf("+")
+    if (this.input.toString().lastIndexOf("-") > pos) pos=this.input.lastIndexOf("-")
+    if (this.input.toString().lastIndexOf("*") > pos) pos=this.input.lastIndexOf("*")
+    if (this.input.toString().lastIndexOf("/") > pos) pos=this.input.lastIndexOf("/")
+    console.log('Last '+this.input.substr(pos+1))
+    return this.input.substr(pos+1)
+  }
+ 
+ 
+  pressOperator(op: string) {
+ 
+    //Do not allow operators more than once
+    const lastKey = this.input[this.input.length - 1];
+    if (lastKey === '/' || lastKey === '*' || lastKey === '-' || lastKey === '+')  {
+      return;
+    }
+   
+    this.input = this.input + op
+    this.calcAnswer();
+  }
+ 
+ 
+  clear() {
+    if (this.input !="" ) {
+      this.input=this.input.substr(0, this.input.length-1)
+    }
+  }
+ 
+  //Brisanje rezultata i inputa klikom na x
+  allClear() {
+    this.result = '';
+    this.input = '';
+  }
+ 
+  //Funkcija za racunanje
+  calcAnswer() {
+    let formula = this.input;
+ 
+    let lastKey = formula[formula.length - 1];
+ 
+    if (lastKey === '.')  {
+      formula=formula.substr(0,formula.length - 1);
+    }
+ 
+    lastKey = formula[formula.length - 1];
+ 
+    if (lastKey === '/' || lastKey === '*' || lastKey === '-' || lastKey === '+' || lastKey === '.')  {
+      formula=formula.substr(0,formula.length - 1);
+    }
+ 
+    console.log("Formula " +formula);
+    this.result = eval(formula);
+  }
+ 
+  //Metoda za =
+  //Izjednacava gornji input (unos korisnika) sa dole prikazanim rezultatom i ako je u gornjem inputu rezultat 0
+  //dole je prazan string tj. prazno polje
+  getAnswer() {
+    this.calcAnswer();
+    this.input = this.result;
+    if (this.input=="0") this.input="";
+    this.histr.push(this.result);
+    localStorage.setItem('lista', JSON.stringify(this.histr));
+  }
+
+ 
+
+
+ 
+}
